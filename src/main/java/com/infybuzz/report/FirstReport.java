35e47ec1e7168dbbe9119241b3162a870c443ef5
evgen <parom.evgen@mail.ru>
2023-12-10 15:20:30 +0300

    init

---
+++ src/main/java/com/infybuzz/report/FirstReport.java
@@ -0,0 +1,14 @@
+package com.infybuzz.report;
+
+public class FirstReport {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+		} catch(Exception e) {
+			System.out.println("Exception while creating report");
+		}
+	}
+
+}
