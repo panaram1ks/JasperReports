5ef3ae99fc15e4c32bf726eb83ec6ca9d2c2eb1c
parom <E.Parominskiy@isida.by>
2024-01-09 12:38:30 +0300

    30. Exporting Report In MS Excel Format

--- Setting-up-Fields-For-Table-Data/src/main/java/com/infybuzz/report/CustomReport.java
+++ Setting-up-Fields-For-Table-Data/src/main/java/com/infybuzz/report/CustomReport.java
@@ -2,7 +2,13 @@ package com.infybuzz.report;
 
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,10 +53,18 @@ public class CustomReport {
 //            JasperPrint print = JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
             JasperPrint print = JasperFillManager.fillReport(report, parameters, chartDataSource);
 
-            // Export to necessary format
+            // Export to PDF format
             JasperExportManager.exportReportToPdfFile(print, "/home/parom/IdeaProjects/JasperReports/Setting-up-Fields-For-Table-Data/src/main/resources/studRep.pdf");
 
+            // Export to HTML format
             JasperExportManager.exportReportToHtmlFile(print, "/home/parom/IdeaProjects/JasperReports/Setting-up-Fields-For-Table-Data/src/main/resources/studRep.html");
+
+            // Export to Excel format
+            JRXlsxExporter exporter = new JRXlsxExporter();
+            exporter.setExporterInput(new SimpleExporterInput(print));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(new FileOutputStream(new File("/home/parom/IdeaProjects/JasperReports/Setting-up-Fields-For-Table-Data/src/main/resources/studRep.xlsx"))));
+            exporter.exportReport();
+
 
             System.out.println("Report Create...");
 
